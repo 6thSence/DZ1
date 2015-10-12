80a9265cc34bf4e994dec2263caea15ca215226f
6thSence <d.pushkarskaya.pr@gmail.com>
2015-10-12 19:48:33 +0600

    Подключено внешнее пространство имен к gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -21,9 +21,9 @@ var gulp = require('gulp'),
 
 //включаем bower файлы 
 gulp.task('bower', function () {
-  gulp.src('./app/*.html')
+  gulp.src(RS_CONF.path.htmlDir)
     .pipe(wiredep({
-      derictory: "app/bower_components",
+      derictory: RS_CONF.psth.baseDir+"/bower_components",
       overrides: {
         "qtip2": {
           "main": ["./jquery.qtip.min.js", "./jquery.qtip.min.css"],
@@ -33,7 +33,7 @@ gulp.task('bower', function () {
         , ignorePath: /^(\.\.\/)*\.\./
     }
     }))
-    .pipe(gulp.dest('./app'));
+    .pipe(gulp.dest(RS_CONF.path.baseDir));
 });
 
 gulp.task('watch', function (){
@@ -44,7 +44,7 @@ gulp.task('watch', function (){
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "./app/"
+            baseDir: RS_CONF.path.baseDir
 
         }
     });
@@ -73,7 +73,7 @@ gulp.task("useref", function () {
 
 // Очищаем директорию DIST
 gulp.task("clean-dist", function () {
-    return del(["./dist/**", "!./dist/"]);
+    return del(RS_CONF.path.distDelDir);
 });
 
 
@@ -82,44 +82,41 @@ gulp.task("dist-server", function () {
     browserSync.init({
         notify: false,
         port: 2000,
-        server: { baseDir: "./dist" }
+        server: { baseDir: RS_CONF.path.distDir }
     });
 });
 
 // Перенос шрифтов
 gulp.task("fonts", function() {
-    // gulp.src("./app/fonts/*")   
-    gulp.src("./app/css/*")
+    gulp.src(RS_CONF.path.baseDir + "/css/*")
         .pipe(filter(["*.eot","*.svg","*.ttf","*.woff","*.woff2"]))
-        .pipe(gulp.dest("./dist/fonts/"))
+        .pipe(gulp.dest(RS_CONF.path.distDir + "/fonts/"))
 });
 
 // Перенос картинок
 gulp.task("images", function () {
-    // return gulp.src("./app/img/**/*")
-    return gulp.src(imgDir)
+    return gulp.src(RS_CONF.path.baseDir+"/css/image/**/*")
             .pipe(imagemin({
                 progressive: true,
                 interlaced: true
             }))
-            .pipe(gulp.dest("./dist/css/image"));
-            // .pipe(gulp.dest("./dist/img"));
+            .pipe(gulp.dest(RS_CONF.path.distDir+"/css/image"));
 });
 
 // Перенос остальных файлов (favicon и т.д.)
 gulp.task("extras", function () {
-    return gulp.src(["./app/*.*", "!./app/*.html"])
-            .pipe(gulp.dest("./dist"));
+    return gulp.src([RS_CONF.path.baseDir+"/*.*", "!"+RS_CONF.path.htmlDir])
+            .pipe(gulp.dest(RS_CONF.path.distDir));
 });
 
 // Вывод размера папки APP
 gulp.task("size-app", function () {
-    return gulp.src("app/**/*").pipe(size({title: "APP size: "}));
+    return gulp.src(RS_CONF.path.baseDir+"/**/*").pipe(size({title: "APP size: "}));
 });
 
 // Сборка и вывод размера папки DIST
 gulp.task("dist", ["useref", "images", "fonts", "extras", "size-app", "dist-server"], function () {
-    return gulp.src("dist/**/*").pipe(size({title: "DIST size: "}));
+    return gulp.src(RS_CONF.path.distDir+"/**/*").pipe(size({title: "DIST size: "}));
 });
 
 // Собираем папку DIST - только когда файлы готовы
